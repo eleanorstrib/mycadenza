40877768c7a96db8ab5bd88bf03f43e8f3586177
eleanorstrib <hello@eleanorstrib.com>
2017-01-16 17:22:30 -0800

    password reset done, all templates working

--- mycadenza/mycadenza/urls.py
+++ mycadenza/mycadenza/urls.py
@@ -4,13 +4,19 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^logout/$', auth_views.logout,{
+                                        'template_name':'registration/logout.html'
+                                        },
+                                        name='logout'),
+    url(r'^password_reset/$', auth_views.password_reset, {
+                                'template_name': 'registration/password_reset.html'
+                                },
+                                name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^admin/', admin.site.urls),
     url('', include('signup.urls')),
     url('', include('reports.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
