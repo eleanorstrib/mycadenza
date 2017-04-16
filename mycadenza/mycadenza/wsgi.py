be8c4d2273e90a372d31d640e07dcf8949bab7a8
eleanorstrib <hello@eleanorstrib.com>
2017-04-15 22:15:23 -0700

    added whitenoise settings and verified static dirs

--- mycadenza/mycadenza/wsgi.py
+++ mycadenza/mycadenza/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for mycadenza project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mycadenza.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
