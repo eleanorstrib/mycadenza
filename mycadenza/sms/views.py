54ae5f139ee59c9859fa5b2859006956084704af
eleanorstrib <hello@eleanorstrib.com>
2017-04-16 02:37:22 -0700

    changes to the views in sms

--- mycadenza/sms/views.py
+++ mycadenza/sms/views.py
@@ -2,15 +2,11 @@ from django.shortcuts import render
 
 from twilio import twiml
 from django_twilio.decorators import twilio_view
-#
+
 @twilio_view
 def sms(request):
     user_msg = request.POST.get('Body', '')
     confirm_msg = "Thanks!  Your entry has been recorded on Cadenza."
-    empty_msg = "Hmm. The entry you sent is empty.  Try again?"
     response = twiml.Response()
-    if user_msg != '':
-        response.message(confirm_msg)
-    else:
-        response.message(empty_msg)
+    response.message(confirm_msg)
     return response
