5b253cb28961473c6122ed231d7b0add2b38ad2c
eleanorstrib <hello@eleanorstrib.com>
2017-04-16 02:52:00 -0700

    testing data

--- mycadenza/sms/views.py
+++ mycadenza/sms/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
-from twilio import twiml
 from django_twilio.decorators import twilio_view
+from twilio.twiml import Response
 
 @twilio_view
 def sms(request):
+    user_phone = request.POST.get('From', '')
     user_msg = request.POST.get('Body', '')
-    confirm_msg = "Thanks!  Your entry has been recorded on Cadenza."
-    response = twiml.Response()
+    confirm_msg = user_phone
+    response = Response()
     response.message(confirm_msg)
     return response
