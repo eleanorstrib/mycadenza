510fafc44a55e15abc82c8e26cd3b06c7590eacc
eleanorstrib <hello@eleanorstrib.com>
2017-04-16 02:29:01 -0700

    finished rolling back v of twilio

--- mycadenza/sms/views.py
+++ mycadenza/sms/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from twilio import twiml
 from django_twilio.decorators import twilio_view
 #
-# @twilio_view
+@twilio_view
 def sms(request):
     user_msg = request.POST.get('Body', '')
     confirm_msg = "Thanks!  Your entry has been recorded on Cadenza."
