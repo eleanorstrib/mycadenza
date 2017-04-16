696bc0c56737baaf8a5194fbded4e76f097113d7
eleanorstrib <hello@eleanorstrib.com>
2017-04-16 03:09:52 -0700

    updated script to check if user mobile in the db

--- mycadenza/sms/views.py
+++ mycadenza/sms/views.py
@@ -3,11 +3,19 @@ from django.shortcuts import render
 from django_twilio.decorators import twilio_view
 from twilio.twiml import Response
 
+from signup.models import CadenzaUser
+
+
 @twilio_view
 def sms(request):
-    user_phone = request.POST.get('From', '')
-    user_msg = request.POST.get('Body', '')
-    confirm_msg = user_phone
+    user_mobile = request.POST.get('From', '')
+    user_in_db = CadenzaUser.objects.filter(mobile=user_mobile)
     response = Response()
-    response.message(confirm_msg)
+    if user_in_db:
+        user_msg = request.POST.get('Body', '')
+        response.message("Thank you - your entry has been recorded.")
+    else:
+        response.message(
+            "You have the wrong number - please remove this one from your phone."
+            )
     return response
