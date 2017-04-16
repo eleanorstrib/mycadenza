8d0bef0852b83fabb3eeb4fa38c8c0d47bbe6d6e
eleanorstrib <hello@eleanorstrib.com>
2017-04-16 02:27:53 -0700

    using older version of twilio helper lib

--- mycadenza/sms/views.py
+++ mycadenza/sms/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-# from django_twilio.decorators import twilio_view
 from twilio import twiml
-
+from django_twilio.decorators import twilio_view
+#
 # @twilio_view
 def sms(request):
     user_msg = request.POST.get('Body', '')
