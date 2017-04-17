96a162b91cfefaabc547582091858d841a9bbf31
eleanorstrib <hello@eleanorstrib.com>
2017-04-16 19:41:44 -0700

    added view with twilio api call to get user messages, confirmed this works, not in browser yet

--- mycadenza/reports/views.py
+++ mycadenza/reports/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+import os
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from twilio.rest import TwilioRestClient
+
+from signup.models import CadenzaUser
+
+ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
+AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
+TWILIO_PH_NO = os.environ.get('TWILIO_PH_NO', '')
+
+
+@login_required
 def dashboard(request):
+    user = request.user
+    print(user)
+    user_data = CadenzaUser.objects.get(username=user)
+    user_mobile_obj = user_data.mobile
+    user_mobile_no = "+" + str(user_mobile_obj.country_code) + str(user_mobile_obj.national_number)
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    all_entries = client.messages.list(
+        from_=user_mobile_no,
+        to=TWILIO_PH_NO,
+    )
+    print(all_entries)
     return render(request, 'reports/dashboard.html', {})
